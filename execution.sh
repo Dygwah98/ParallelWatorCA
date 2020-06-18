537c61a71337483af4e3090e2bf32056785ef8f2
Davide Caligiuri <36772959+Dygwah98@users.noreply.github.com>
2020-06-19 00:00:31 +0200

    Update execution.sh

--- execution.sh
+++ execution.sh
@@ -4,8 +4,8 @@ dir="$1"
 np="$2"
 
 cd "$dir"/WatorGUI
-sudo qmake WatorGUI.pro
-sudo make -j4
-sudo make clean
+sudo qmake WatorGUI.pro > /dev/null
+sudo make -j4 > /dev/null
+sudo make clean > /dev/null
 mpirun -n "$np" ./WatorGUI
 sudo rm -f WatorGUI
