a08128f063449179ab9d4316ae54943c5f53ebb1
Davide Caligiuri <36772959+Dygwah98@users.noreply.github.com>
2020-06-20 01:07:52 +0200

    Update main.cc

--- parallel-no-gui/main.cc
+++ parallel-no-gui/main.cc
@@ -12,10 +12,10 @@ int main(int argc, char const *argv[]) {
 
 	MPI_Init(NULL, NULL);
 
-    int world_rank, world_size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-    srand(time(NULL) * world_rank);
+    	int world_rank, world_size;
+    	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    	srand(time(NULL) * world_rank);
 
 	Cell **mat = NULL;
 	Cell **tam = NULL;
@@ -30,31 +30,27 @@ int main(int argc, char const *argv[]) {
 
 	int extra = 0;
 	allocateMatrix(&mat, dim, &matdata, dim);
-    if(world_rank == 0) {
-    	extra = dim - p*world_size;
-    	allocateMatrix(&tam, p + extra, &tamdata, dim);
-    	sendbufp = &( mat[ extra ][ 0 ] );
-    } else {
-    	allocateMatrix(&tam, p, &tamdata, dim);
-    }
+    	if(world_rank == 0) {
+    		extra = dim - p*world_size;
+    		allocateMatrix(&tam, p + extra, &tamdata, dim);
+		sendbufp = &( mat[ extra ][ 0 ] );
+    	} else {
+    		allocateMatrix(&tam, p, &tamdata, dim);
+    	}
 
-    int pos = 0;
-    if(world_rank == 0)
-    	pos = extra;
+    	for(int i = 0; i < iter; ++i) {  
 
-    for(int i = 0; i < iter; ++i) {  
+    		MPI_Barrier(MPI_COMM_WORLD);
+    		MPI_Bcast(&(mat[0][0]), dim, vec_t, 0, MPI_COMM_WORLD);
 
-    	MPI_Barrier(MPI_COMM_WORLD);
-    	MPI_Bcast(&(mat[0][0]), dim, vec_t, 0, MPI_COMM_WORLD);
+    		runWator(mat, tam, p + extra, dim, p*world_rank, 0);
 
-    	runWator(mat, tam, p + extra, dim, p*world_rank, 0);
-
-    	MPI_Gather(&(tam[pos][0]), p, vec_t, sendbufp, p, vec_t, 0, MPI_COMM_WORLD);
-    }
+    		MPI_Gather(&(tam[extra][0]), p, vec_t, sendbufp, p, vec_t, 0, MPI_COMM_WORLD);
+    	}
     
 	freeMatrix(mat, matdata);
-    freeMatrix(tam, tamdata);
+    	freeMatrix(tam, tamdata);
     
-    MPI_Finalize();
+    	MPI_Finalize();
 	return 0;
 }
