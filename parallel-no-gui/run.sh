d9008a38d3cdfa27305a20779e7b22b3103b71e7
Dygwah98 <ypyudc@gmail.com>
2020-06-22 22:45:33 +0200

    building scripts updated

--- parallel-no-gui/run.sh
+++ parallel-no-gui/run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-dir="$1"
 np="$2"
 nt="$3"
 dim="$4"
@@ -10,6 +9,6 @@ sudo env OMP_DYNAMIC=false > /dev/null
 sudo env OMP_NUM_THREADS="$nt" > /dev/null
 sudo env OMP_PROC_BIND=master > /dev/null
 sudo env OMP_PLACES=core > /dev/null
-sudo mpicxx -fopenmp -o Wator.out "$dir"/*.cc #> /dev/null 2>&1
+sudo mpicxx -fopenmp -o Wator.out *.cc #> /dev/null 2>&1
 sudo nice -n -20 mpiexec --bind-to core -n "$np" nice -n -20 ./Wator.out "$dim" "$iter" "$nt"
 sudo rm -f Wator.out
--- temp.sh
+++
@@ -1,7 +0,0 @@
-sudo sh measurements_script.sh 1000 100 output
-sudo sh measurements_script.sh 2500 100 output
-sudo sh measurements_script.sh 5000 100 output
-sudo sh measurements_script.sh 7500 100 output
-sudo sh measurements_script.sh 10000 100 output
-sudo sh measurements_script.sh 12500 100 output
-sudo sh measurements_script.sh 15000 100 output
